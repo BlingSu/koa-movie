cd0bd41cf20237e6d17cf18603dd1185fef6defb
angelasubi <21978301@qq.com>
2018-05-02 16:53:18 +0800

    feat: pug tpl

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const Koa = require('koa')
 const app = new Koa()
-const {normal} = require('./template/index')
+const {pugTpl} = require('./template')
+const pug = require('pug')
 
 app.use(async (ctx, next) => {
   ctx.header = 'text/html; charset=utf-8'
-  ctx.body = normal
+  ctx.body = pug.render(pugTpl, {
+    you: 'pig',
+    me: 'angelasu'
+  })
 })
 
 app.listen(2333)
