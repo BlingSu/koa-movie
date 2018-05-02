d4811b6824716747aec7b344c83ccc968f18a243
angelasubi <21978301@qq.com>
2018-05-02 16:09:20 +0800

    feat: html template

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa')
 const app = new Koa()
+const {normal} = require('./template/index')
 
 app.use(async (ctx, next) => {
-  ctx.body = 'hello koa-movie'
+  ctx.header = 'text/html; charset=utf-8'
+  ctx.body = normal
 })
 
 app.listen(2333)
