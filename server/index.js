bfc5156e9480bda4205297200c3826653453cc7a
angelasubi <21978301@qq.com>
2018-05-02 15:56:37 +0800

    refactor: init project

--- server/index.js
+++ server/index.js
@@ -1,25 +1,8 @@
 const Koa = require('koa')
-const logger = require('koa-logger')
-const session = require('koa-session')
 const app = new Koa()
 
-app.keys = ['Hi lick']
-
-app.use(logger())
-app.use(session(app))
-
-app.use(ctx => {
-  if (ctx.path === '/favicon.ico') return
-  if (ctx.path == '/') {
-    let n = ctx.session.views || 0
-    ctx.session.views = ++n
-    ctx.body = n + ' 次'
-  } else if (ctx.path == '/hi') {
-    ctx.body = 'hi'
-  } else {
-    ctx.body = '404'
-  }
+app.use(async (ctx, next) => {
+  ctx.body = 'hello koa-movie'
 })
-
 
 app.listen(2333)
