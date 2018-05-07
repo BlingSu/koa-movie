71cc22eadc6b8417f7814b078f78771db02b34bf
angelasubi <21978301@qq.com>
2018-05-07 18:02:56 +0800

    feat; userSchema

---
+++ server/database/schema/movie.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Mixed = Schema.Types.Mixed
+
+const movieSchema = new Schema({
+  doubanId: String,
+  rate: Number,
+  title: String,
+  summary: String,
+  video: String,
+  poster: String,
+  cover: String,
+
+  videoKey: String,
+  posterKey: String,
+  coverKey: String,
+
+  rawTitle: String,
+  movieTypes: [String],
+  pubdate: Mixed,
+  year: Number,
+
+  tags: [String],
+
+  meta: {
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    updateAt: {
+      type: Date,
+      default: Date.now()
+    }
+  }
+})
+
+movieSchema.pre('save', next => {
+  if (this.isNew) {
+    this.meta.createdAt = this.meta.updateAt = Date.now()
+  } else {
+    this.meta.updateAt = Date.now()
+  }
+  next()
+})
+
+
+mongoose.model('Movie', movieSchema)
