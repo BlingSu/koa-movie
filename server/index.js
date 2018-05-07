8f9672fd19a83f8660429a1ff8fd519bbf95d787
angelasubi <21978301@qq.com>
2018-05-07 18:39:01 +0800

    feat: categorySchema

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const Koa = require('koa')
+const mongoose = require('mongoose')
 const app = new Koa()
 const views = require('koa-views')
 const {resolve} = require('path')
-const {connect} = require('./database/init')
+const {connect, initSchemas} = require('./database/init')
 
 ;(async () => {
   await connect()
+
+  initSchemas()
+
+  const Movie = mongoose.model('Movie')
+  const movies = await Movie.find({})
+
+  console.log(movies)
 })()
 
 
